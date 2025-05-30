f6ccb537bcbae56359166ff19384b2fb09851a35
[Archibald] <[archpod@gmail.com]>
2025-05-30 14:25:38 +0300

    added Session

--- main.py
+++ main.py
@@ -19,51 +19,42 @@ token_create_endpoint = "https://test-blog-api-vmxh.onrender.com/auth/token/logi
 post_list_endpoint = "https://test-blog-api-vmxh.onrender.com/posts/"
 
 
-def get_access_token():
-    """
-    Возвращает токен
-    для аутентификации в системе.
-    """
-    data = {
-        "username": userdata["username"],
-        "password": userdata["password"]
-    }
-    # Преобразуем объект Response в словарь Python
-    # и получаем значение ключа auth_token
-    response = requests.post(token_create_endpoint, data=data).json()
-    return response['auth_token']
-
-
 @app.route('/')
 def get_all_posts():
-    """
-    Возвращает все опубликованные посты
-    """
+    with requests.Session() as session:
 
-    access_token = get_access_token()
-    headers = {"Authorization": f"token {access_token}",
-               "Accept": "application/json"
-               }
-    response = requests.get(post_list_endpoint,
-                            headers=headers)
-    statement = f"INSERT INTO {TABLE_NAME}(title, body, owner) values(?, ?, ?)"
+        data = {
+            "username": userdata["username"],
+            "password": userdata["password"]
+        }
 
-    try:
-        for el in response.json():
-            cursor.execute(statement,
-                           (el['title'], el['description'], el['author']))
-            connection.commit()
+        response = session.post(token_create_endpoint, data=data).json()
+        access_token = response['auth_token']
 
-        response = app.response_class(
-            response=json.dumps({'STATUS': 'OK'}),
-            status=200,
-            mimetype='application/json'
-        )
-        return response
-    except json.JSONDecodeError as e:
-        print(f'Error: {e}')
-        return f'Некорректный ответ от сервера'
+        headers = {"Authorization": f"token {access_token}",
+                   "Accept": "application/json"
+                   }
 
+        response = session.get(post_list_endpoint,
+                               headers=headers,
+                               )
+        statement = f"INSERT INTO {TABLE_NAME}(title, body, owner) values(?, ?, ?)"
+
+        try:
+            for el in response.json():
+                cursor.execute(statement,
+                               (el['title'], el['description'], el['author']))
+                connection.commit()
+
+            response = app.response_class(
+                response=json.dumps({'STATUS': 'OK'}),
+                status=200,
+                mimetype='application/json'
+            )
+            return response
+        except json.JSONDecodeError as e:
+            print(f'Error: {e}')
+            return f'Некорректный ответ от сервера'
 
 if __name__ == '__main__':
     app.run(debug=True)
